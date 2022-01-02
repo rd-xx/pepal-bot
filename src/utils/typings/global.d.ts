2ea8ede310037848c79a493f6c09fcab8e478d74
rdx <reydux95@gmail.com>
2022-01-02 23:00:48 +0100

    Typings

---
+++ src/utils/typings/global.d.ts
@@ -0,0 +1,19 @@
+/**
+ * Typings globaux pour l'autocomplete.
+ */
+declare global {
+	namespace NodeJS {
+		interface ProcessEnv {
+			ENVIRONMENT: string | 'dev' | 'prod';
+			DISCORD_TOKEN: string;
+
+			DB_HOST: string | 'db';
+			DB_PORT: string;
+			DB_USERNAME: string;
+			DB_PASSWORD: string;
+			DB_NAME: string;
+		}
+	}
+}
+
+export {};
