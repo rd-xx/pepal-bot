0d607bc51b921741a93228b59e77bd05d915d517
rdx <reydux95@gmail.com>
2022-01-02 23:01:38 +0100

    Client du bot

---
+++ src/structure/client.ts
@@ -0,0 +1,76 @@
+import { AkairoClient, ListenerHandler } from 'discord-akairo';
+import { Connection, createConnection } from 'typeorm';
+import { join } from 'path';
+
+// -------------------------------------------------- \\
+
+export default class Client extends AkairoClient {
+	#config: BotOptions;
+	db?: Connection;
+
+	// --------------- \\
+
+	listenerHandler = new ListenerHandler(this, {
+		directory: join(__dirname, '..', 'listeners')
+	});
+
+	// --------------- \\
+
+	constructor(config: BotOptions) {
+		super({
+			intents: [
+				'DIRECT_MESSAGES',
+				'GUILDS',
+				'GUILD_MESSAGES',
+				'GUILD_INTEGRATIONS'
+			],
+			partials: ['CHANNEL'],
+			presence: { status: 'dnd' },
+			ownerID: config.developers
+		});
+
+		this.#config = config;
+	}
+
+	// --------------- \\
+
+	private async _init(): Promise<void> {
+		this.listenerHandler.setEmitters({
+			listenerHandler: this.listenerHandler,
+			process
+		});
+
+		const connection = await createConnection({
+			type: 'postgres',
+			host: process.env.DB_HOST,
+			port: Number(process.env.DB_PORT),
+			username: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			// ##### \\
+			cache: true
+		});
+		await connection.close();
+		await connection.connect();
+		await connection
+			.query('SELECT id FROM guilds;')
+			.catch(async () => await connection.synchronize());
+
+		this.listenerHandler.loadAll();
+		this.db = connection;
+	}
+
+	// --------------- \\
+
+	async start(): Promise<string> {
+		await this._init();
+		return this.login(this.#config.token);
+	}
+}
+
+// -------------------------------------------------- \\
+
+interface BotOptions {
+	token?: string;
+	developers?: string | string[];
+}
