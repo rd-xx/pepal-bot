6e5d7b45d2850410693cf0895a136ff126061cba
rdx <reydux95@gmail.com>
2022-01-05 07:37:25 +0100

    Prochain cours

--- src/commands/tt.command.ts
+++ src/commands/tt.command.ts
@@ -1,6 +1,8 @@
+import { Message, MessageEmbed } from 'discord.js';
+import { palette } from '../utils/constants';
 import { Command } from 'discord-akairo';
 import Pepal from '../structure/pepal';
-import { Message } from 'discord.js';
+import { DateTime } from 'luxon';
 
 // -------------------------------------------------- \\
 
@@ -14,6 +16,50 @@ export default class TimeTableCommand extends Command {
 	async exec(message: Message): Promise<Message> {
 		const pepal = await Pepal.init(message.author.id);
 		await pepal.getTimeTable();
-		return message;
+
+		const futureLessons = pepal.timeTable
+			.filter(
+				(lesson) =>
+					DateTime.local().setZone('Europe/Paris').toMillis() < +lesson.start
+			)
+			.sort((a, b) => +a.start - +b.start);
+
+		if (!futureLessons.length)
+			return message.reply(
+				"Il n'y a plus de cours ! Bonnes vacances ; on se revoit l'année prochaine."
+			);
+
+		const relativeTimeOne = DateTime.fromJSDate(
+				futureLessons[0].start
+			).toRelative({
+				locale: 'fr'
+			}),
+			embed = new MessageEmbed().setColor(palette.success).addField(
+				'Prochain cours',
+				`**Matière** : ${futureLessons[0].discipline}
+			${
+				futureLessons[0].professor
+					? `**Intervenant** : ${futureLessons[0].professor}\n`
+					: ''
+			}**Salle** : ${futureLessons[0].room}
+			${
+				relativeTimeOne
+					? relativeTimeOne.replace(
+							relativeTimeOne.charAt(0),
+							relativeTimeOne.charAt(0).toUpperCase()
+					  ) +
+					  ` pendant ${
+							futureLessons[0].end.getHours() -
+							futureLessons[0].start.getHours()
+					  } heures`
+					: ''
+			}`,
+				true
+			);
+
+		if (this.client.user && this.client.user.avatarURL())
+			embed.setThumbnail(this.client.user.avatarURL() as string);
+
+		return message.reply({ embeds: [embed] });
 	}
 }
